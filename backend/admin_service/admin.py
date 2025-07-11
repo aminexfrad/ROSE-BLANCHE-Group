001c5d1358f08c1c9fbf9b1b26b74e14cf0bfdd9
Mohamed Amine FRAD <frad.amine@gmail.com>
2025-07-11 02:03:25 +0100

    chore: remove PFEProject admin registration to hide from Django admin dashboard

--- backend/admin_service/admin.py
+++ backend/admin_service/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
 from .models import PFEProject
 
-@admin.register(PFEProject)
-class PFEProjectAdmin(admin.ModelAdmin):
-    list_display = ('reference_id', 'title', 'diplome', 'specialite', 'nombre_postes', 'ville')
-    search_fields = ('reference_id', 'title', 'specialite', 'ville')
-    fields = ('reference_id', 'title', 'description', 'objectives', 'keywords', 'diplome', 'specialite', 'nombre_postes', 'ville') 
+# PFEProject admin registration removed to hide from Django admin dashboard 
